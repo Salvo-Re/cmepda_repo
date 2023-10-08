659f27b84ca515790cc6efcccb8f06a004c77b8e
Salvo-Re <sasa.santoro@gmail.com>
2023-10-08 21:14:33 +0200

    corrections

--- test.py
+++ test.py
@@ -1,4 +1,18 @@
-import square
+from square import square
+import numpy as np
+
+'''Modulo di test
+'''
 
 def test_square():
-    assert square(2.) == 4
+    '''Testa la funzione square del file square
+    '''
+    assert square(3) == 9
+    assert square(2.) == 4.
+    assert square(-2.) == 4.
+    assert square(0.) == 0.
+    assert square(2) == 4
+    assert square(-2) == 4
+    assert square(0) == 0
+    assert np.allclose(square(np.ones(100)), np.ones(100))
+    print('yeee')
